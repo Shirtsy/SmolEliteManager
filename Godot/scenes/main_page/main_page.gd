3b5678510bbc7d2e2fb545f4837d85498c506415
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2025-04-08 14:08:05 -0400

    Updated

---
+++ Godot/scenes/main_page/main_page.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+@export var url: String:
+	get:
+		assert(url, "This property cannot be null.")
+		return url
+
+@onready var http: HTTPRequest = $HTTPRequest
+@onready var request_timer: Timer = $RequestTimer
+
+
+func _ready() -> void:
+	make_request()
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_request_timer_timeout() -> void:
+	make_request()
+
+
+func _on_http_request_request_completed(
+	_result: int,
+	_response_code: int,
+	_headers: PackedStringArray,
+	body: PackedByteArray
+) -> void:
+	var body_text: String = body.get_string_from_utf8()
+	var body_data: Dictionary = JSON.parse_string(body_text)
+	var new_label: Label = Label.new()
+	new_label.text = body_text
+	$Margin/Panel/Margin/HBox/ScrollContainer2/VBoxContainer.add_child(new_label)
+	request_timer.start()
+
+
+func make_request() -> void:
+	var error: Error = http.request(url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
