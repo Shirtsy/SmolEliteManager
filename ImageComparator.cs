c0b1f73de1e21aac30c5e2be59a121895f1e18cd
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2025-04-14 16:57:20 -0400

    Updates

---
+++ ImageComparator.cs
@@ -0,0 +1,12 @@
+﻿using SixLabors.ImageSharp;
+
+public static class ImageComparator
+{
+    public static float CompareImages(Image imageA, Image imageB)
+    {
+        return 5f;
+    }
+
+    public static float CompareImages(String imagePathA, String imagePathB) =>
+        CompareImages(Image.Load(imagePathA), Image.Load(imagePathB));
+}
